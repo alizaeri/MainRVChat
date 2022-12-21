1f0bac4ede66e2d2d154b4c0ce5d3fc8d0966468
alizaeri <90989935+alizaeri@users.noreply.github.com>
2022-12-21 11:44:00 +0100

    call pick upp is fixed

--- lib/screens/mobile_layout_screen.dart
+++ lib/screens/mobile_layout_screen.dart
@@ -5,6 +5,7 @@ import 'package:rvchat/colors.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:rvchat/features/auth/controller/auth_controller.dart';
 import 'package:rvchat/features/call/controller/call_controller.dart';
+import 'package:rvchat/features/call/screens/call_pickup_screen.dart';
 import 'package:rvchat/features/call/screens/call_screen.dart';
 import 'package:rvchat/features/select_contacts/screens/select_contact_screen.dart';
 import 'package:rvchat/models/call.dart';
@@ -63,129 +64,56 @@ class _MobileLayoutScreenState extends ConsumerState<MobileLayoutScreen>
   ];
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DocumentSnapshot>(
-      stream: ref.watch(callControllerProvider).callStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.data() != null) {
-          Call call =
-              Call.fromMap(snapshot.data!.data() as Map<String, dynamic>);
+    return CallPickupScreen(scaffold: scaffold());
+  }
 
-          if (!call.hasDialled) {
-            return Scaffold(
-              body: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Incoming Call',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(call.callerPic),
-                      radius: 60,
-                    ),
-                    const SizedBox(height: 50),
-                    Text(
-                      call.callerName,
-                      style: const TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    const SizedBox(height: 75),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.call_end,
-                              color: Colors.redAccent),
-                        ),
-                        const SizedBox(width: 25),
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CallScreen(
-                                  channelId: call.callId,
-                                  call: call,
-                                  isGroupChat: false,
-                                ),
-                              ),
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.call,
-                            color: Colors.green,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }
-        }
-        return Scaffold(
-          body: _pageNo[selectedPage],
-          bottomNavigationBar: ConvexAppBar.badge(
-            const {3: '99+'},
-            items: [
-              TabItem(
-                icon: ImageIcon(const AssetImage("assets/icons/list.png"),
-                    color: white.withOpacity(0.5)),
-                activeIcon: const ImageIcon(AssetImage("assets/icons/list.png"),
-                    color: pinkL1),
-                title: "List",
-              ),
-              TabItem(
-                  icon: ImageIcon(const AssetImage("assets/icons/fave.png"),
-                      color: white.withOpacity(0.5)),
-                  activeIcon: const ImageIcon(
-                      AssetImage("assets/icons/fave.png"),
-                      color: pinkL1),
-                  title: "search"),
-              TabItem(
-                  icon: ImageIcon(const AssetImage("assets/icons/rv.png"),
-                      color: white.withOpacity(0.5)),
-                  activeIcon: const ImageIcon(AssetImage("assets/icons/rv.png"),
-                      color: pinkL1),
-                  title: "RVChat"),
-              TabItem(
-                  icon: ImageIcon(const AssetImage("assets/icons/chat.png"),
-                      color: white.withOpacity(0.5)),
-                  activeIcon: const ImageIcon(
-                      AssetImage("assets/icons/chat.png"),
-                      color: pinkL1),
-                  title: "Chat"),
-              TabItem(
-                  icon: ImageIcon(const AssetImage("assets/icons/prof.png"),
-                      color: white.withOpacity(0.5)),
-                  activeIcon: const ImageIcon(
-                      AssetImage("assets/icons/prof.png"),
-                      color: pinkL1),
-                  title: "Profile")
-            ],
-            backgroundColor: pinkL1,
-            initialActiveIndex: selectedPage,
-            onTap: (int index) {
-              setState(() {
-                selectedPage = index;
-                print(selectedPage);
-              });
-            },
+  Scaffold scaffold() {
+    return Scaffold(
+      body: _pageNo[selectedPage],
+      bottomNavigationBar: ConvexAppBar.badge(
+        const {3: '99+'},
+        items: [
+          TabItem(
+            icon: ImageIcon(const AssetImage("assets/icons/list.png"),
+                color: white.withOpacity(0.5)),
+            activeIcon: const ImageIcon(AssetImage("assets/icons/list.png"),
+                color: pinkL1),
+            title: "List",
           ),
-        );
-      },
+          TabItem(
+              icon: ImageIcon(const AssetImage("assets/icons/fave.png"),
+                  color: white.withOpacity(0.5)),
+              activeIcon: const ImageIcon(AssetImage("assets/icons/fave.png"),
+                  color: pinkL1),
+              title: "search"),
+          TabItem(
+              icon: ImageIcon(const AssetImage("assets/icons/rv.png"),
+                  color: white.withOpacity(0.5)),
+              activeIcon: const ImageIcon(AssetImage("assets/icons/rv.png"),
+                  color: pinkL1),
+              title: "RVChat"),
+          TabItem(
+              icon: ImageIcon(const AssetImage("assets/icons/chat.png"),
+                  color: white.withOpacity(0.5)),
+              activeIcon: const ImageIcon(AssetImage("assets/icons/chat.png"),
+                  color: pinkL1),
+              title: "Chat"),
+          TabItem(
+              icon: ImageIcon(const AssetImage("assets/icons/prof.png"),
+                  color: white.withOpacity(0.5)),
+              activeIcon: const ImageIcon(AssetImage("assets/icons/prof.png"),
+                  color: pinkL1),
+              title: "Profile")
+        ],
+        backgroundColor: pinkL1,
+        initialActiveIndex: selectedPage,
+        onTap: (int index) {
+          setState(() {
+            selectedPage = index;
+            print(selectedPage);
+          });
+        },
+      ),
     );
   }
 }
